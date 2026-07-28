87c161a86e7973e3ef35b824c925fa93ea09c5e4
lex <lex@work.cn>
2026-07-28 12:16:57 +0800

    fix: move close hint to dialog title border

--- lua/poste/sql/help.lua
+++ lua/poste/sql/help.lua
@@ -140,10 +140,9 @@ function M.open()
   end
   table.sort(close_parts, function(a, b) return #a < #b end)
   local close_text = #close_parts > 0 and table.concat(close_parts, " / ") or "q"
-  table.insert(lines, "  " .. close_text .. "  close")
 
   local d = dialog.open({
-    title = " Poste SQL Keymaps ",
+    title = string.format(" Poste SQL Keymaps   [%s] close ", close_text),
     width = width,
     height = math.min(#lines, vim.o.lines - 4),
   })
